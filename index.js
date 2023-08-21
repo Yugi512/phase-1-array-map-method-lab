f1c2f2ced30b1d2ea319b356a65f8d2081143010
Kelvin Gil <gilkelvin2@gmail.com>
2023-08-21 10:41:54 -0400

    finished lab

--- index.js
+++ index.js
@@ -11,6 +11,19 @@ const tutorials = [
   'what is JSONP?'
 ];
 
+// i looked online to find a way to access each word individually and one way was to use the .split method 
+// so we got the code to seperate the words and then capitalize the first word of every word now we have to make it so that they are all together rather than in an array 
+
 const titleCased = () => {
-  return tutorials
+  return tutorials.map((strings) => {
+      let seperateWords = strings.split(" ");
+      return seperateWords.map((words) => {
+        const firstLetterUpperCase = words[0].toUpperCase();
+        const restOfTheWords = words.slice(1);
+        let combinedStrings = firstLetterUpperCase.concat(restOfTheWords);
+        return combinedStrings;
+    }).join(" ");
+  });
 }
+// currently all the words are seperated and the first letters are capitalized
+// so we access the inside of the arrays again and then combined the words again
